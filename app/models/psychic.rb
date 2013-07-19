c78e89c58530d23d7ca93745120a53c27acdb631
Felipe Coury <felipe.coury@gmail.com>
2013-07-19 08:35:41 -0300

    shows calls in Psychic area

--- app/models/psychic.rb
+++ app/models/psychic.rb
@@ -4,6 +4,7 @@ class Psychic < ActiveRecord::Base
   belongs_to :user
   delegate :first_name, :last_name, :full_name, to: :user
   has_many :preferred_by_clients, class_name: "Client", foreign_key: "favorite_psychic_id"
+  has_many :calls, class_name: "ClientCall"
 
   before_create :assign_extension
   validates_uniqueness_of :extension
