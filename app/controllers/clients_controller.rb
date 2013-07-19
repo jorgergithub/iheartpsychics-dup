0a6cb907fcf2e5dd6a62806008a8cf18aec01d6b
Felipe Coury <felipe.coury@gmail.com>
2013-07-18 21:56:32 -0300

    client dashboard almost functional

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,5 +1,34 @@
 class ClientsController < AuthorizedController
+  before_filter :find_client
+
   def show
-    @client = current_user.client
+  end
+
+  def add_minutes
+    logger.info "Added minutes: #{params[:client].inspect}"
+    if params[:client]
+      if @client.add_minutes(params[:client][:minutes])
+        redirect_to client_path, notice: "Minutes added to your account."
+      else
+        render action: "add_minutes"
+      end
+    end
+  end
+
+  def reset_pin
+    if params[:client]
+      @client.pin = params.require(:client).permit(:pin)
+      if @client.save
+        redirect_to client_path, notice: "Your PIN has been reset."
+      else
+        render action: "reset_pin"
+      end
+    end
+  end
+
+  protected
+
+  def find_client
+    @client = current_client
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,21 @@ IHeartPsychics::Application.routes.draw do
 
   resources :calls do
     collection do
-      post 'notify(:.format)' , action: :notify
-      get  'notify(:.format)' , action: :notify
-      post 'user(:.format)'   , action: :user
-      post 'pin(:.format)'    , action: :pin
+      post 'notify(:.format)'         , action: :notify
+      get  'notify(:.format)'         , action: :notify
+      post 'user(:.format)'           , action: :user
+      post 'pin(:.format)'            , action: :pin
+      post 'call_finished(:.format)'  , action: :call_finished
     end
   end
 
-  resource :clients do
-    resource :phones, controller: 'client_phones' do
+  resource :client do
+    resources :client_phones, shallow: true
+    member do
+      get   'reset_pin(:.format)'   , action: :reset_pin, as: :reset_pin
+      patch 'reset_pin(:.format)'   , action: :reset_pin
+      get   'add_minutes(:.format)' , action: :add_minutes, as: :add_minutes
+      patch 'add_minutes(:.format)' , action: :add_minutes
     end
   end
 
