ed0ec9c0adb4f2b11d72dd19bce5f3c6c1931ac1
Felipe Coury <felipe.coury@gmail.com>
2013-07-18 21:55:03 -0300

    allows client to maintain phone numbers from UI

--- app/controllers/client_phones_controller.rb
+++ app/controllers/client_phones_controller.rb
@@ -1,8 +1,44 @@
 class ClientPhonesController < AuthorizedController
+  before_filter :find_phone
+
   def new
+    @phone = ClientPhone.new
+  end
+
+  def edit
+  end
+
+  def create
+    @phone = current_client.phones.new(phone_params)
+    if @phone.save
+      redirect_to client_path, notice: "New phone was successfully created."
+    else
+      render action: "show"
+    end
   end
 
   def update
-    render text: params
+    if @phone.update_attributes(phone_params)
+      redirect_to client_path, notice: "Phone was successfully updated."
+    else
+      render action: "show"
+    end
+  end
+
+  def destroy
+    @phone.destroy
+
+    redirect_to client_path, notice: 'Game was successfully deleted.'
+  end
+
+  protected
+
+  def find_phone
+    @client = current_client
+    @phone = @client.phones.find(params[:id]) if params[:id]
+  end
+
+  def phone_params
+    params.require(:client_phone).permit(:number, :desc)
   end
 end
