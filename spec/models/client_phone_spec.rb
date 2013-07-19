88aa343afd968a8c8311150cd71c3ffddfbac475
Felipe Coury <felipe.coury@gmail.com>
2013-07-18 21:44:26 -0300

    make sure phone number is formatted before saving

--- spec/models/client_phone_spec.rb
+++ spec/models/client_phone_spec.rb
@@ -7,4 +7,17 @@ describe ClientPhone do
       expect(phone.formatted_number).to eql("+1-786-329-5532")
     end
   end
+
+  describe "saving a number" do
+    let(:client) { FactoryGirl.create(:client) }
+    it "removes formatting" do
+      phone = client.phones.create(number: "+1-305-450-2995")
+      expect(phone.number).to eql("+13054502995")
+    end
+
+    it "inserts +1 if needed" do
+      phone = client.phones.create(number: "305-450-2995")
+      expect(phone.number).to eql("+13054502995")
+    end
+  end
 end
