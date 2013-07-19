59c815494b453cea9e880fa7d00b07417af4b1c6
Felipe Coury <felipe.coury@gmail.com>
2013-07-18 21:52:53 -0300

    added current_client helper

--- app/controllers/authorized_controller.rb
+++ app/controllers/authorized_controller.rb
@@ -1,3 +1,8 @@
 class AuthorizedController < ApplicationController
   before_filter :authenticate_user!
+
+  def current_client
+    return unless current_user and current_user.client?
+    current_user.client
+  end
 end
