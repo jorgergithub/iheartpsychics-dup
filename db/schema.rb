7123a02579dbaad95e242af643cd349ebd44968b
Felipe Coury <felipe.coury@gmail.com>
2013-07-18 23:46:31 -0300

    added extension to Psychics

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130719020140) do
+ActiveRecord::Schema.define(version: 20130719024504) do
 
   create_table "client_calls", force: true do |t|
     t.integer  "client_id"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20130719020140) do
     t.string   "phone"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "extension"
   end
 
   create_table "users", force: true do |t|
