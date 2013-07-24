7e8e46adfb3a669fbdbd0f38cf4963ae6c13d0fc
Felipe Coury <felipe.coury@gmail.com>
2013-07-23 22:01:11 -0300

    added email options

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,13 @@ IHeartPsychics::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    :address              => "0.0.0.0",
+    :port                 => 1025 }
 end
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,4 +33,6 @@ IHeartPsychics::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+
+  config.action_mailer.default_url_options = { :host => 'localhost' }
 end
