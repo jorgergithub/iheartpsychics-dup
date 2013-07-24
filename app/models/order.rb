03c676378c6aef86ab695955bccb1b7dd1ed8f1c
Felipe Coury <felipe.coury@gmail.com>
2013-07-24 16:07:33 -0300

    calculate order total when saving

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,8 @@ class Order < ActiveRecord::Base
   has_many :items, class_name: "OrderItem"
   has_many :transactions
 
+  before_save :calculate_total
+
   def add_package_item(pkg)
     items.create(package: pkg, qty: 1, unit_price: pkg.price,
                  total_price: pkg.price, description: pkg.name)
@@ -12,5 +14,11 @@ class Order < ActiveRecord::Base
     transaction = transactions.create(operation: "charge")
     charge = client.charge(total, "Order ##{id}")
     transaction.update_attributes success: true, transaction_id: charge.id
+  end
+
+  private
+
+  def calculate_total
+    self.total = items.inject(0) { |t, i| t += i.total_price }
   end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -4,6 +4,15 @@ describe Order do
   let(:package) { FactoryGirl.create(:package) }
   let(:order) { FactoryGirl.create(:order) }
 
+  describe "saving the order" do
+    it "sets the total to the sum of items" do
+      order.items.build(description: "item 1", qty: 1, unit_price: 10, total_price: 10)
+      order.items.build(description: "item 2", qty: 4, unit_price: 7.5, total_price: 30)
+      order.save
+      expect(order.total).to eql(40.0)
+    end
+  end
+
   describe "#pay" do
     let(:stripe_client) { double(:stripe_client).as_null_object }
     let(:client) { order.client }
