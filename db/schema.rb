1031f408ec203ddca55fd0e69afbd795512f647c
Felipe Coury <felipe.coury@gmail.com>
2013-07-23 22:02:36 -0300

    user needs to confirm email while signing up

--- db/schema.rb
+++ db/schema.rb
@@ -101,8 +101,13 @@ ActiveRecord::Schema.define(version: 20130719164801) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "username"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
