ff1b8ce97e4a23276bf499baaaaf8d9d3404d01a
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2013-09-05 17:50:29 -0300

    Fixed phone mask and parser

--- app/parsers/phone_parser.rb
+++ app/parsers/phone_parser.rb
@@ -2,12 +2,17 @@ module PhoneParser
   extend self
 
   def parse(value)
-    valid_for_parsing?(value) ? remove_dashes(value) : value
+    if valid_for_parsing?(value)
+      value = remove_dashes(value)
+      value = set_us_international_code(value)
+    end
+
+    value
   end
 
   def localize(value)
     if value =~ /^\+(\d)(\d{3})(\d{3})(\d{4})$/
-      "+#{$1}-#{$2}-#{$3}-#{$4}"
+      "#{$2}-#{$3}-#{$4}"
     else
       value
     end
@@ -15,11 +20,15 @@ module PhoneParser
 
   protected
 
+  def set_us_international_code(value)
+    "+1#{value}"
+  end
+
   def remove_dashes(value)
     value.gsub("-", "")
   end
 
   def valid_for_parsing?(value)
-    value =~ /\+1-\d{3}-\d{3}-\d{4}/
+    value =~ /\d{3}-\d{3}-\d{4}/
   end
 end
