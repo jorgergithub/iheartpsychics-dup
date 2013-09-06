d3fc7f70b029377b304ac39103d5414e42c378d1
Felipe Coury <felipe.coury@gmail.com>
2013-09-06 00:52:02 -0300

    first steps for OrdersNew specs

--- spec/javascripts/module/order/orders_new.spec.js
+++ spec/javascripts/module/order/orders_new.spec.js
@@ -1,11 +1,40 @@
 describe("IHP.Pages.OrdersNew", function() {
-  var ordersNew, container;
+  var container, submit, cancel;
+  var package, payment, orders_new;
 
   beforeEach(function() {
-    container = $("<div><form></form></div>");
-    ordersNew = IHP.Pages.OrdersNew(container);
+    container = $("<div/>").appendTo(".output")
+    form = $("<form onsubmit='return false;' />").appendTo(container);
+    submit = $("<button class='go'>").appendTo(form);
+    cancel = $("<a class='cancel'>").appendTo(form);
 
-    event = $.Event();
+    package = jasmine.createSpyObj("package", ["validate"]);
+    payment = jasmine.createSpyObj("payment", ["validate"]);
+
+    spyOn(IHP.Pages.Orders, "Package").and.callReturn(package);
+    spyOn(IHP.Pages.Orders, "Payment").and.callReturn(payment);
+
+    orders_new = IHP.Pages.OrdersNew(container);
   });
 
+  describe("when the form is submitted", function() {
+    var event;
+
+    beforeEach(function() {
+      event = $.Event;
+      event.type = "submit";
+    });
+
+    it("validates the package", function() {
+      form.trigger(event);
+      expect(package.validate).toHaveBeenCalled();
+    });
+
+    it("validates the payment", function() {
+      package.validate.and.callReturn(true);
+
+      form.trigger(event);
+      expect(payment.validate).toHaveBeenCalled();
+    });
+  });
 });
