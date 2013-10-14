3c9ee327eaf45f4113d08a9578307e04f8555b29
Felipe Coury <felipe.coury@gmail.com>
2013-10-13 21:13:53 -0300

    [design] is seems IE hates Clamp.js :(

--- app/assets/javascripts/clamp.js
+++ app/assets/javascripts/clamp.js
@@ -3,7 +3,13 @@
   // adds clamp as a jQuery plugin
   $.fn.clamp = function(clamp_arg) {
     return this.each(function() {
-      $clamp(this, {clamp: clamp_arg});
+
+      // IE hates Clamp.js :/
+      try {
+        $clamp(this, {clamp: clamp_arg});
+      }
+      catch (e) {}
+
     });
   };
 
