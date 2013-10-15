0951fab13fd8be4cc011a795021718a989ad11c0
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2013-10-14 23:01:52 -0300

    Improved the way tabs work
    When you click on a tab and reload the page, now it goes back to the
    page you were.
    
    [Finishes #58480478]

--- app/assets/javascripts/boot.js
+++ app/assets/javascripts/boot.js
@@ -1,7 +1,25 @@
 $(document).ready(function() {
   var body = $("body");
   var page = body.data("page");
+
   if (page) {
     Module.run("IHP.Pages." + page, body);
   }
+
+  $('.nav-tabs a').on('shown', function (e) {
+    if (history.pushState) {
+      history.pushState(null, null, e.target.hash);
+    } else {
+      window.location.hash = e.target.hash;
+    }
+  })
+
+  if (location.hash) {
+    $('a[href=' + location.hash + ']').tab('show');
+  }
+});
+
+$(window).on('popstate', function() {
+  var anchor = location.hash || $("a[data-toggle=tab]").first().attr("href");
+  $('a[href=' + anchor + ']').tab('show');
 });
