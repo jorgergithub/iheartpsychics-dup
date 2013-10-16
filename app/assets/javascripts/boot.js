22457049113a8ec1869c6c172c54f914ddd3fc9e
Felipe Coury <felipe.coury@gmail.com>
2013-10-15 23:34:12 -0300

    extracted tab handling JS code to tabs.js

--- app/assets/javascripts/boot.js
+++ app/assets/javascripts/boot.js
@@ -5,21 +5,4 @@ $(document).ready(function() {
   if (page) {
     Module.run("IHP.Pages." + page, body);
   }
-
-  $('.nav-tabs a').on('shown', function (e) {
-    if (history.pushState) {
-      history.pushState(null, null, e.target.hash);
-    } else {
-      window.location.hash = e.target.hash;
-    }
-  })
-
-  if (location.hash) {
-    $('a[href=' + location.hash + ']').tab('show');
-  }
-});
-
-$(window).on('popstate', function() {
-  var anchor = location.hash || $("a[data-toggle=tab]").first().attr("href");
-  $('a[href=' + anchor + ']').tab('show');
 });
--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -9,4 +9,21 @@ $(document).ready(function() {
     $target.tab('show');
     return false;
   });
+
+  $('.nav-tabs a').on('shown', function (e) {
+    if (history.pushState) {
+      history.pushState(null, null, e.target.hash);
+    } else {
+      window.location.hash = e.target.hash;
+    }
+  })
+
+  if (location.hash) {
+    $('a[href=' + location.hash + ']').tab('show');
+  }
+});
+
+$(window).on('popstate', function() {
+  var anchor = location.hash || $("a[data-toggle=tab]").first().attr("href");
+  $('a[href=' + anchor + ']').tab('show');
 });
