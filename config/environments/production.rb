03051759c845139dabd5cec9f471019d432fd285
Felipe Coury <felipe.coury@gmail.com>
2013-11-11 17:39:07 -0500

    gets the Mailer config from ENV

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,5 @@ IHeartPsychics::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'iheartpsychics.co' }
+  config.action_mailer.default_url_options = { :host => ENV['MAILER_HOST'] || 'iheartpsychics.co' }
 end
