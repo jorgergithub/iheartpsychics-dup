f7c271be0ccd25084b83c0cc6e2e7a06d655b244
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2013-11-13 20:26:41 -0200

    Allow psychic to set his alias but disallow to change
    On the admin area and on the psychic application it should be able to
    edit.
    
    [Finishes #60714316]

--- app/controllers/psychics_controller.rb
+++ app/controllers/psychics_controller.rb
@@ -53,16 +53,14 @@ class PsychicsController < AuthorizedController
 
   def psychic_params
     params.require(:psychic).permit(:phone, :ability_clairvoyance, :ability_clairaudient,
-      :ability_clairsentient, :ability_empathy, :ability_medium,
-      :ability_channeler, :ability_dream_analysis, :tools_tarot,
-      :tools_oracle_cards, :tools_runes, :tools_crystals, :tools_pendulum,
-      :tools_numerology, :tools_astrology, :specialties_love_and_relationships,
-      :specialties_career_and_work, :specialties_money_and_finance,
-      :specialties_lost_objects, :specialties_dream_interpretation,
-      :specialties_pet_and_animals, :specialties_past_lives,
-      :specialties_deceased, :style_compassionate, :style_inspirational,
-      :style_straightforward, :about, :price, :pseudonym,
-      schedules_attributes: [:id, :date, :start_time_string, :end_time_string,
-        :_destroy] )
+      :ability_clairsentient, :ability_empathy, :ability_medium, :ability_channeler,
+      :ability_dream_analysis, :tools_tarot, :tools_oracle_cards, :tools_runes,
+      :tools_crystals, :tools_pendulum, :tools_numerology, :tools_astrology,
+      :specialties_love_and_relationships, :specialties_career_and_work,
+      :specialties_money_and_finance, :specialties_lost_objects,
+      :specialties_dream_interpretation, :specialties_pet_and_animals,
+      :specialties_past_lives, :specialties_deceased, :style_compassionate,
+      :style_inspirational, :style_straightforward, :about, :price,
+      schedules_attributes: [:id, :date, :start_time_string, :end_time_string, :_destroy])
   end
 end
