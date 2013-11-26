867f60899d47891e4eb3614b5c4f81bbb614a9aa
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2013-11-26 04:01:20 -0200

    Added default avatar image to client and psychic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -64,6 +64,14 @@ module ApplicationHelper
     provider.to_s.titleize
   end
 
+  def avatar_image_tag(image_id)
+    if image_id.present?
+      cl_image_tag(@client.avatar_id, :width => 265, :height => 265, :crop => :thumb, :gravity => 'faces:center', :radius => :max)
+    else
+      image_tag("avatar.png", alt: "Edit Your Picture")
+    end
+  end
+
   private
 
   def admin_controller?
