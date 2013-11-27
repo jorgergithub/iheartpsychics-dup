228e8df9c4f404c297aa720479026dfbec43fbef
Felipe Iketani <felipeiketani@gmail.com>
2013-11-26 23:48:08 -0300

    added validation errors to pin modal

--- app/assets/javascripts/main/modules/client/modals/pin.js
+++ app/assets/javascripts/main/modules/client/modals/pin.js
@@ -5,11 +5,18 @@ Module("IHP.Modals.PinModal", function(PinModal) {
     this.el = $("#pin_modal", el);
     this.pin = $("#client_pin", el);
     this.assign();
+    this.bindDismiss();
   };
 
   PinModal.fn.assign = function() {
     this.pin.val(this.el.attr("data-pin"));
     this.pin.select();
     this.pin.focus();
+  };
+
+  PinModal.fn.bindDismiss = function() {
+    this.el.on("dismiss", function() {
+      $("#pin_modal").find(".form_validation_errors").remove();
+    });
   };
 });
