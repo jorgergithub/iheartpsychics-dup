e7d337c2ac921e760be485fd1496234b4a0451f8
Felipe Iketani <felipeiketani@gmail.com>
2013-11-27 20:38:09 -0300

    show new credit card modal when client has no card

--- app/assets/javascripts/main/modules/client/modals/payment_method.js
+++ app/assets/javascripts/main/modules/client/modals/payment_method.js
@@ -3,10 +3,14 @@ Module("IHP.Modals.SelectPaymentMethodModal", function(SelectPaymentMethodModal)
 
   SelectPaymentMethodModal.fn.initialize = function(attributes) {
     this.el = $("#select_payment_method_modal");
-    this.package = attributes.package;
+    
+    this.selectCardPanel    = $(".select_card_panel", this.el);
+    this.creditCardsPanel   = $(".credit_cards_panel", this.el);
+    this.paypalPanel        = $(".paypal_panel", this.el);
+    this.newCreditCardPanel = $(".new_credit_card_panel", this.el);
+    this.package            = attributes.package;
 
     this.assign();
-    this.setPackage();
     this.initNewCreditCardPayment();
     this.bindNavigation();
     this.cardSelection();
@@ -14,7 +18,22 @@ Module("IHP.Modals.SelectPaymentMethodModal", function(SelectPaymentMethodModal)
 
   SelectPaymentMethodModal.fn.assign = function() {
     $(".new_credit_card_panel form", this.el)[0].reset();
+    this.setPackage();
+    this.showFirstPanel();
   };
+
+  SelectPaymentMethodModal.fn.showFirstPanel = function() {
+    var cards = $("ul.credit_cards li", this.selectCardPanel);
+    if (cards.length > 0) {
+      $("a[data-sub-panel='select_card_panel']", this.newCreditCardPanel).show();
+      this.selectCardPanel.show();
+      this.newCreditCardPanel.hide();    
+    } else {
+      $("a[data-sub-panel='select_card_panel']", this.newCreditCardPanel).hide();
+      this.selectCardPanel.hide();
+      this.newCreditCardPanel.show();
+    }
+  }
 
   SelectPaymentMethodModal.fn.setPackage = function() {
     $(".package-credits", this.el).html(this.package.credits);
@@ -34,10 +53,16 @@ Module("IHP.Modals.SelectPaymentMethodModal", function(SelectPaymentMethodModal)
       $(this).addClass("tab_selected");
     });
 
-    this.el.off("click", "li[data-panel], a[data-panel]").on("click", "li[data-panel], a[data-panel]", function() {
+    this.el.off("click", "nav li[data-panel]").on("click", "nav li[data-panel]", function() {
       var panel = $(this).data("panel");
       $(".modal_panel").hide();
       $(".modal_panel." + panel).show();
+    });
+
+    this.el.off("click", "a[data-sub-panel]").on("click", "a[data-sub-panel]", function() {
+      var panel = $(this).data("sub-panel");
+      $(".sub_modal_panel").hide();
+      $(".sub_modal_panel." + panel).show();
     });
   };
 
