a9416e0719862c4d1c01f558e63b67256fa1fb3d
Felipe Iketani <felipeiketani@gmail.com>
2013-11-27 01:16:10 -0300

    WIP paypal

--- app/assets/javascripts/main/modules/client/modals/payment_method.js
+++ app/assets/javascripts/main/modules/client/modals/payment_method.js
@@ -3,7 +3,7 @@ Module("IHP.Modals.SelectPaymentMethodModal", function(SelectPaymentMethodModal)
 
   SelectPaymentMethodModal.fn.initialize = function(attributes) {
     console.log("SelectPaymentMethodModal", attributes);
-    var el = $("#select_payment_method")
+    var el = $("#select_payment_method_modal");
 
     this.el = el;
     this.package = attributes.package;
@@ -11,7 +11,11 @@ Module("IHP.Modals.SelectPaymentMethodModal", function(SelectPaymentMethodModal)
     console.log("el", el);
     console.log("credits", this.package.credits);
 
-    $(".package-credits").html(this.package.credits);
-    $(".package-price").html(this.package.price);
+    $(".package-credits", el).html(this.package.credits);
+    $(".package-price", el).html(this.package.price);
+
+    el.data("package-id", this.package.id);
+
+    el.find("#paypal_checkout input[type=hidden]").val(this.package.id)
   };
 });
