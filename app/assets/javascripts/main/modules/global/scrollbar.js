ce31039a7d4d989956911b7e1f00e6d73500c527
Felipe Iketani <felipeiketani@gmail.com>
2013-12-02 17:06:17 -0300

    fixed scroll bar table when table is smaller than 450 px

--- app/assets/javascripts/main/modules/global/scrollbar.js
+++ app/assets/javascripts/main/modules/global/scrollbar.js
@@ -1,10 +1,33 @@
 Module("IHP.Components.Scrollbar", function(Scrollbar) {
   "use strict";
 
-  Scrollbar.fn.initialize = function() {
-    $(".scrollbar_table").tinyscrollbar({ size: 400 });
+  Scrollbar.fn.MAX_HEIGHT = 450;
 
+  Scrollbar.fn.initialize = function() {
+    this.elements = $(".scrollbar_table");
+    
+    this.assign();
     this.addEventListeners();
+  };
+
+  Scrollbar.fn.assign = function() {
+    $.each(this.elements, function(index, element) {
+      var viewport = $(".viewport", element);
+
+      var tableHeight = $("table", element).height();
+      var options = {}
+
+      if (tableHeight > this.MAX_HEIGHT) {
+        viewport.css("height", "450px");
+        options["size"] = this.MAX_HEIGHT - 50
+        
+      } else {
+        viewport.css("height", tableHeight + "px");
+        options["size"] = 0
+      };
+
+      $(element).tinyscrollbar(options);
+    }.bind(this));
   };
 
   Scrollbar.fn.addEventListeners = function() {
