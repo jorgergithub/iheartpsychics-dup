75cb501a9c2914af386e0d7c6833daba568803e5
Felipe Iketani <felipeiketani@gmail.com>
2013-12-02 20:21:28 -0300

    psychic can manage featured reviews on dashboard

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,18 +2,26 @@ class ReviewsController < AuthorizedController
   before_filter :find_review
 
   def mark_as_featured
-    if @review.mark_as_featured!
-      redirect_to edit_psychic_path, notice: "Review successfully marked as featured."
-    else
-      redirect_to :back, error: "Could not mark as featured."
+    respond_to do |format|
+      if @review.mark_as_featured!
+        format.html { redirect_to edit_psychic_path, notice: "Review successfully marked as featured." }
+        format.js
+      else
+        format.html { redirect_to :back, error: "Could not mark as featured." }
+        format.js
+      end
     end
   end
 
   def unmark_as_featured
-    if @review.unmark_as_featured!
-      redirect_to edit_psychic_path, notice: "Review successfully unmarked as featured."
-    else
-      redirect_to :back, error: "Could not unmark as featured."
+    respond_to do |format|
+      if @review.unmark_as_featured!
+        format.html { redirect_to edit_psychic_path, notice: "Review successfully unmarked as featured." }
+        format.js
+      else
+        format.html { redirect_to :back, error: "Could not unmark as featured." }
+        format.js
+      end
     end
   end
 
--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -10,36 +10,57 @@ describe ReviewsController do
   }
 
   describe "GET mark_as_featured" do
-    before { get :mark_as_featured, id: review.id }
 
-    it "redirects back to the psychic page" do
-      expect(response).to redirect_to(edit_psychic_path)
+    context "as js" do
+      before { get :mark_as_featured, id: review.id, format: :js }
+      
+      it "renders success" do
+        expect(response).to render_template :mark_as_featured
+      end
     end
 
-    it "sets a flash message" do
-      expect(flash[:notice]).to eql("Review successfully marked as featured.")
+    context "as html" do
+      before { get :mark_as_featured, id: review.id }
+      it "redirects back to the psychic page" do
+        expect(response).to redirect_to(edit_psychic_path)
+      end
+
+      it "sets a flash message" do
+        expect(flash[:notice]).to eql("Review successfully marked as featured.")
+      end
     end
 
     it "makes the review featured" do
+      get :mark_as_featured, id: review.id
       expect(review.reload).to be_featured
     end
   end
 
   describe "GET unmark_as_featured" do
-    before {
-      review.update_attributes featured: true
-      get :unmark_as_featured, id: review.id
-    }
+    before { review.update_attributes featured: true }
 
-    it "redirects back to the psychic page" do
-      expect(response).to redirect_to(edit_psychic_path)
+    context "as js" do
+      before { get :unmark_as_featured, id: review.id, format: :js }
+
+      it "renders success" do
+        expect(response).to render_template :unmark_as_featured
+      end
     end
 
-    it "sets a flash message" do
-      expect(flash[:notice]).to eql("Review successfully unmarked as featured.")
+    context "as html" do
+      before { get :unmark_as_featured, id: review.id }
+
+      it "redirects back to the psychic page" do
+        expect(response).to redirect_to(edit_psychic_path)
+      end
+
+      it "sets a flash message" do
+        expect(flash[:notice]).to eql("Review successfully unmarked as featured.")
+      end
     end
 
     it "makes the review featured" do
+      get :unmark_as_featured, id: review.id
       expect(review.reload).to_not be_featured
     end
   end
