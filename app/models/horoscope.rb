70e3fcd4ff666da56034427b1fbef0cf426ca44a
Felipe Coury <felipe.coury@gmail.com>
2013-12-03 14:45:02 -0500

    fixed Scorpio and Sagitarius backwards
    [Finishes #61808600]

--- app/models/horoscope.rb
+++ app/models/horoscope.rb
@@ -1,6 +1,6 @@
 class Horoscope < ActiveRecord::Base
-  SIGNS = [ Sign::Aries, Sign::Taurus, Sign::Gemini, Sign::Cancer, Sign::Leo, Sign::Virgo, 
-            Sign::Libra, Sign::Sagittarius, Sign::Scorpio, Sign::Capricorn, Sign::Aquarius, Sign::Pisces ]
+  SIGNS = [ Sign::Aries, Sign::Taurus, Sign::Gemini, Sign::Cancer, Sign::Leo, Sign::Virgo,
+            Sign::Libra, Sign::Scorpio, Sign::Sagittarius, Sign::Capricorn, Sign::Aquarius, Sign::Pisces ]
 
   scope :last_by_date,  lambda { order("date DESC").first }
   scope :last_lovescope_horoscope, lambda { where("lovescope <> ''").last_by_date }
