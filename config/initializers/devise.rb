4e8d648a1664915d889689dd4d0fbacff9202252
Douglas Barbosa Alexandre <dbalexandre@gmail.com>
2013-12-05 22:01:13 -0200

    Fix devise emails use the right email address
    [Finishes #61946366]

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class with default "from" parameter.
-  config.mailer_sender = "noreply@iheartpsychics.co"
+  config.mailer_sender = "noreply@iheartpsychics.com"
 
   # Configure the class responsible to send e-mails.
   # config.mailer = "Devise::Mailer"
