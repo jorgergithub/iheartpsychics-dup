183fee6181efad7ae908d2ebc69d93bb566cb01c
Felipe Iketani <felipeiketani@gmail.com>
2013-12-10 19:46:23 -0300

    registration pixel

--- app/views/registrations/create.js
+++ app/views/registrations/create.js
@@ -1,1 +1,5 @@
+$("#registration_pixel_conversion").remove()
+var marinConversionPixel = "<%= escape_javascript render('registrations/registration_conversion_pixel') %>";
+$(marinConversionPixel).insertAfter($("#sign_up_success_modal"));
+
 showModal("sign_up_success_modal");
