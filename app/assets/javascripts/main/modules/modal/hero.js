7395a5e0b638615f9325fb59f0f3aeb4e0a04763
Felipe Iketani <felipeiketani@gmail.com>
2013-12-13 16:23:11 -0300

    enlarged hero popup

--- app/assets/javascripts/main/modules/modal/hero.js
+++ app/assets/javascripts/main/modules/modal/hero.js
@@ -9,7 +9,7 @@ Module("IHP.Modals.HeroModal", function(Modal) {
   };
 
   Modal.fn.video = function() {
-    return $('<iframe width="640" height="360" src="//www.youtube.com/embed/FneaWvYc8Xs?rel=0&autoplay=1&controls=0&showinfo=0&rel=0" frameborder="0" allowfullscreen></iframe>');
+    return $('<iframe width="853" height="480" src="//www.youtube.com/embed/FneaWvYc8Xs?rel=0&autoplay=1&controls=0&showinfo=0&rel=0" frameborder="0" allowfullscreen></iframe>');
   };
 
   Modal.fn.addEventListeners = function() {
