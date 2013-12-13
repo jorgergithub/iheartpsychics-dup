6916740af3e2d195a7c6c8e7b571a1430a2ada82
Wagner J Goncalves <wagnerjgoncalves@gmail.com>
2013-12-12 23:38:27 -0200

    Edit Psychic phone number at Psychics Profile Information
    [Finishes #62420018]

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -32,6 +32,7 @@
 //= require main/modules/horoscope/index
 //= require main/modules/psychic/about
 //= require main/modules/psychics/about
+//= require main/modules/psychic/modals/edit_profile
 //= require main/modules/psychic/schedule
 //= require main/modules/psychic/show
 //= require main/modules/modal/forgot_password
