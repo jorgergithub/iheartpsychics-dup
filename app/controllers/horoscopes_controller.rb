2e0b707c47db0921ef20689deae762882982f48f
Felipe Coury <felipe.coury@gmail.com>
2014-01-31 10:49:03 -0200

    Fixed problem opening the payment popup in Horoscopes

--- app/controllers/horoscopes_controller.rb
+++ app/controllers/horoscopes_controller.rb
@@ -1,9 +1,10 @@
 class HoroscopesController < ApplicationController
   layout "main"
-  
+
   before_action :build_new_user
 
   def index
+    @client = current_client
     @horoscope = Horoscope.last_by_date
     @lovescope_horoscope = Horoscope.last_lovescope_horoscope
   end
